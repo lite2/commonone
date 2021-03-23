d5a5d91c5641d50e06d1eab24baf79cb6b4467c0
Lai Wai Liang <lai_wai_liang@moe.edu.sg>
2021-03-23 21:18:13 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,40 +1,11 @@
 import flask
+from flask import render_template
 
 app = flask.Flask(__name__)
 
 @app.route('/')
-def apple():
-    return 'Routed to index()'
-
-@app.route('/css')
-def css():
-    return 'Routed to css()'
-
-@app.route('/no_slash')
-def no_slash():
-    return 'Routed to no_slash()'
-
-@app.route('/optional_slash/')
-def optional_slash():
-    return 'Routed to optional_slash()'
-
-@app.route('/one/')
-@app.route('/one/two/')
-@app.route('/three/two/one')
-def multiple():
-    return 'Routed to multiple()'
-
-@app.route('/string/<s>/')
-def string_variable(s):
-    return 'Routed to string_variable(), s = {}'.format(s)
-
-@app.route('/integer/<int:i>/')
-def integer_variable(i):
-    return 'Routed to integer_variable(), i = {}'.format(i)
-
-@app.route('/bryan/', methods=['POST'])
-def post_only():
-    return 'Routed to post_only()'
+def home():
+    return render_template('stylish.html')
 
 if __name__ == '__main__':
     app.run()
